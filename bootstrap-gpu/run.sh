5150e714b1d20f5309fc8a2c4b904b3727ac02cb
Dmitry Lunin <mail.lunin@gmail.com>
2020-01-17 06:58:24 +0300

    Initial commit.

---
+++ bootstrap-gpu/run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+PROJECT_DIR=$SCRIPT_DIR/..
+CONTAINER_SCRIPT_DIR=${SCRIPT_DIR/$HOME/\/home/ubuntu}
+
+USER_ID=$(id -u ${USER})
+USER_NAME=$(getent group "$(id -g ${USER})" | cut -d: -f1)
+
+COLOR='\u001b[38;5;208;1m'
+NO_COLOR='\033[0m'
+
+pushd $SCRIPT_DIR
+
+echo -e "${COLOR}Building the Exosuit bootstrap-gpu container${NO_COLOR}"
+
+sudo docker build \
+	--build-arg PROJECT_PATH=$PROJECT_DIR \
+	. -t exosuit-bootstrap-gpu
+
+if [ $? -eq 0 ]; then
+    echo -e "${COLOR}Build successful${NO_COLOR}"
+else
+    echo -e "${COLOR}Build failed${NO_COLOR}"
+	exit 1
+fi
+
+echo -e "${COLOR}Launching the Exosuit bootstrap-gpu container as user '$USER_NAME'${NO_COLOR}"
+
+sudo docker run --rm -it -v $HOME:$HOME \
+	-v $PROJECT_DIR:$PROJECT_DIR \
+	-w $SCRIPT_DIR/../../ \
+	-v "/etc/group:/etc/group:ro" \
+	-v "/etc/passwd:/etc/passwd:ro" \
+	-v "/etc/shadow:/etc/shadow:ro" \
+	-v /var/run/docker.sock:/var/run/docker.sock \
+	-u $USER_ID:$USER_ID \
+	--net=host \
+	exosuit-bootstrap-gpu python3 /home/wrwt/Programming/Exosuit/generic/run.py --gpu --stage=main
+
+popd
---
+++ generic/run.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python3
+import pystache
+import yaml
+import os
+import sys
+import click
+import tempfile
+import docker
+import subprocess
+import colorama
+import re
+
+colorama.init()
+
+def ensure_path(path):
+    path = os.path.expanduser(path)
+    path = os.path.realpath(path)
+    path = os.path.abspath(path)
+    dir_name = os.path.dirname(path)
+    try:
+        os.makedirs(dir_name)
+    except FileExistsError:
+        pass
+
+
+@click.command()
+@click.option('--gpu/--cpu', default=True, help='Enable or disable GPU support and things that rely on it')
+@click.option('--stage', type=click.Choice(['bootstrap', 'main'], case_sensitive=False), default='bootstrap', help='Target stage')
+@click.option('--generate-only', default=False, help='Do not build launch the container, just generate the Dockerfile and the launcher script')
+def run(gpu, stage, generate_only):
+    script_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
+    script_path = os.path.realpath(sys.argv[0])
+    exosuit_dir = os.path.abspath(os.path.expanduser(os.path.join(script_dir, '../')))
+    home_dir = os.path.expanduser('~')
+
+    userid = int(subprocess.run('id -u ${USER}', shell=True, stdout=subprocess.PIPE).stdout)
+    username = subprocess.run('getent group "$(id -g ${USER})" | cut -d: -f1', shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')[:-1]
+
+    with open(os.path.join(script_dir, 'requirements.yaml'), 'r') as f:
+        requirements = yaml.load(f.read(), Loader=yaml.SafeLoader)
+
+    gpu_str = 'gpu' if gpu else 'cpi'
+    container_name = f'{stage}-{gpu_str}'
+    image_tag = f'exosuit-{stage}-{gpu_str}'
+    target_dir = os.path.join(exosuit_dir, container_name)
+
+    if stage == 'bootstrap':
+        container_command = f'python3 {script_path} --{gpu_str} --stage=main'
+    else:
+        container_command = '/bin/bash'
+
+    dockerfile_render_args = {
+        'gpu': gpu,
+        'apt_get_deps': [
+            {'package': package}
+            for package, info in requirements['apt'].items()
+            if stage in info['stages']
+        ],
+        'python_deps': [
+            {
+                'package': package,
+                'version': info['version'],
+            } for package, info in requirements['python'].items() 
+            if stage in info['stages']
+        ],
+        'stage': stage,
+    }
+
+    with open(os.path.join(script_dir, 'Dockerfile.template'), 'r') as f:
+        template_dockerfile = f.read()
+    rendered_dockerfile = pystache.render(template_dockerfile, dockerfile_render_args)
+    rendered_dockerfile = re.sub('\n\n+', '\n\n', rendered_dockerfile)
+
+    dockerfile_path = os.path.join(target_dir, 'Dockerfile')
+    ensure_path(dockerfile_path)
+    with open(dockerfile_path, 'w') as f:
+        f.write(rendered_dockerfile)
+
+    run_script_render_args = {
+        'container_name': container_name,
+        'image_tag': image_tag,
+        'command': container_command,
+    }
+
+    with open(os.path.join(script_dir, 'run.template'), 'r') as f:
+        template_run_script = f.read()
+    rendered_run_script = pystache.render(template_run_script, run_script_render_args)
+    rendered_run_script = re.sub('\n\n+', '\n\n', rendered_run_script)
+
+    run_script_path = os.path.join(target_dir, 'run.sh')
+    ensure_path(run_script_path)
+    with open(run_script_path, 'w') as f:
+        f.write(rendered_run_script)
+
+    os.system(f'sudo chmod +x {run_script_path}')
+    if not generate_only:
+        os.system(f'{run_script_path}')
+
+
+if __name__ == '__main__':
+    run()
